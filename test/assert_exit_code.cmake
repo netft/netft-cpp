35ad5dc923c6f2430d2d15681193669e21e7cc7a
Xudong Han <42986190+han-xudong@users.noreply.github.com>
2026-07-23 15:19:28 +0800

    Harden CLI process test helper

--- test/assert_exit_code.cmake
+++ test/assert_exit_code.cmake
@@ -4,10 +4,17 @@ endif()
 
 execute_process(
   COMMAND "${PROGRAM}" ${ARGUMENTS}
+  TIMEOUT 30
   RESULT_VARIABLE actual_exit_code
   OUTPUT_QUIET
   ERROR_QUIET
 )
+
+if(NOT "${actual_exit_code}" MATCHES "^-?[0-9]+$")
+  message(FATAL_ERROR
+    "Failed to execute ${PROGRAM}: ${actual_exit_code}"
+  )
+endif()
 
 if(NOT actual_exit_code EQUAL EXPECTED_EXIT_CODE)
   message(FATAL_ERROR
